70c61c2790b155a6e87ae37e08923f214f384493
Thomas Dib <s3838765@student.rmit.edu.au>
2023-05-10 09:33:56 +1000

    Base implementation - milk generation and number formatting

---
+++ src/lib/util/numberFormatter.js
@@ -0,0 +1,35 @@
+const units = [
+  { magnitude: 1e-3, unit: 'microlitre' },
+  { magnitude: 1, unit: 'millilitre' },
+  { magnitude: 1e3, unit: 'litre' },
+  { magnitude: 1e6, unit: 'kilolitre' },
+  { magnitude: 1e9, unit: 'megalitre' },
+  { magnitude: 1e12, unit: 'gigalitre' },
+  { magnitude: 1e15, unit: 'teralitre' },
+  // { magnitude: 1e18, unit: 'petalitre' },
+  // { magnitude: 1e21, unit: 'exalitre' },
+  // { magnitude: 1e24, unit: 'zettalitre' },
+]
+
+
+
+/**
+ * Takes a decimal value and formats it in the form "<number> <unit> of milk"
+ * e.g. "3.205 nanolitres of milk"
+ */
+export function formatVolume(value) {
+  if (value === 0) {
+    return "no"
+  }
+  // Check if the current value exists as a key - in this case we don't add an "s" at the end of the unit
+  let sFlag = !units.some((unit) => unit.magnitude === value)
+
+  // Finds the first magnitude where the argument value is higher - this is the most relevant unit
+  for (let i = units.length - 1; i >= 0; i--) {
+    if (Math.abs(value) >= units[i].magnitude) {
+      return `${(value/units[i].magnitude).toPrecision(5)} ${units[i].unit}${sFlag ? 's' : ''}`
+    }
+  }
+
+  return "an unknown amount"
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,59 @@
 <script>
+  import { onMount } from 'svelte'
+  import { formatVolume } from '$lib/util/numberFormatter.js'
+
+  let milkPerClick = 1
+  let milkPerSecond = 0
+  let milk = 0
+
+  /**
+   * Game tick functionality - increments the amount of milk
+   * according to the milk per second, creates a smooth value
+   * transition rather than jumping
+   */
+  let lastTime = null
+  function incrementMilk(timeStamp) {
+    if (!lastTime) lastTime = timeStamp
+    const elapsed = timeStamp - lastTime
+    // Compute how much milk would have been generated since last tick
+    const increment = milkPerSecond * elapsed / 1000
+    // store.update((store) => ({ ...store, milk: store.milk + increment}))
+    if (increment !== 0) {
+      milk += increment
+    }
+    lastTime = timeStamp
+    requestAnimationFrame(incrementMilk)
+  }
+  onMount(() => {
+    requestAnimationFrame(incrementMilk)
+  })
+
+  /**
+   * Milk click functionality - increments milk amount based off your
+   * milkPerClick value.
+   * 
+   * @param power a multiplier for your milkPerClick value
+   */
+  function milkClickFn(power) {
+    milk += milkPerClick * power
+  }
 </script>
+
+{#if milk === 0}
+  <p>You currently have no milk</p>
+{:else}
+  <p>You currently have {formatVolume(milk)} of milk</p>
+{/if}
+
+<p>You are earning {formatVolume(milkPerSecond)} milk per second.</p>
+
+<button on:click={() => milkClickFn(1)}>Milk</button>
+<button on:click={() => milkClickFn(2)}>Milk x2</button>
+<button on:click={() => milkClickFn(5)}>Milk x5</button>
+<button on:click={() => milkClickFn(10)}>Milk x10</button>
+<button on:click={() => milkClickFn(100)}>Milk x100</button>
+<button on:click={() => milkPerSecond += 1}>+1 mps</button>
+<button on:click={() => milkPerSecond += 100}>+100 mps</button>
+<button on:click={() => milkPerSecond += 1000}>+1000 mps</button>
+<button on:click={() => milkPerSecond += 100000}>+100000 mps</button>
+<button on:click={() => milkPerSecond += 10000000}>+1000000 mps</button>999999
